a210c3e2c014449f743db6576ccecc3538e4ea5a
roniardiyanto <roni.ardiyanto@koding.sch.id>
2019-02-05 19:54:03 +0700

    menambah penyimpanan data dengan cookie

--- index.php
+++ index.php
@@ -1,11 +1,36 @@
 <?php
 include_once('header.php');
-include('header.php');
+
+$namaPengguna = "roni";
+$kataSandi = "ardiyanto";
+if (isset($_POST['submit'])) {
+
+    if ($_POST['namaPengguna'] == $namaPengguna &&
+        $_POST['kataSandi'] === $kataSandi
+        ) {
+        #cookie untuk menyimpan data sementara , tergantung pada waktu yang kita berikan(detik)
+            setcookie('namaPengguna', $_POST['namaPengguna'], time()+2000);
+        #untuk memindahkan user ke halaman jika berhasil login atau gagal
+            header('Location: redirect.php?nama=' . $namaPengguna);
+    } else {
+        echo "login gagal";
+    }
+}
+
 ?>
 <main>
     <h2>Halo Selamat Datang</h2>
+    <form action="index.php" method="post">
+            <div>
+                    <input type="text" placeholder="Nama Pengguna" name="namaPengguna">
+            </div>
+            <div>
+                    <input type="password" name="kataSandi">
+            </div>
+            <input class="btn" type="submit" name="submit" value="MASUK">
+
+        </form>
 </main>
 <?php
 include_once('footer.php');
-include('footer.php');
 ?>
