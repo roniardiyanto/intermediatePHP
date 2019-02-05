8fc9781ac4122c5d0d9e0361a280967f9e98f9dd
roniardiyanto <roni.ardiyanto@koding.sch.id>
2019-02-05 20:47:47 +0700

    mengganti cache dengan SESSION dan menambahkan pilihan logout

--- redirect.php
+++ redirect.php
@@ -1,3 +1,11 @@
 <?php
-    echo "Selamat datang " . $_COOKIE['pengguna'];
+session_start();
+
+    if ( isset($_SESSION['pengguna'])) {
+        echo "Nama anda adalah " . $_SESSION['pengguna'];
+    } else {
+        echo "Anda Siapa ?, Silahkan login dulu";
+    }
 ?>
+<br>
+<a href="logout.php">LOGOUT</a>
